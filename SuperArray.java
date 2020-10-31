95139737b2e4e5957185b2fdbb437844b547fa0c
jzhowie <jzhou20@stuy.edu>
2020-10-31 15:52:12 -0400

    Initialized SuperArray and added tester function

---
+++ SuperArray.java
@@ -0,0 +1,14 @@
+public class SuperArray {
+private String[] data;
+private int size;   //current size
+
+public SuperArray() {
+	size = 0;
+	data = new String[10];
+}
+
+//tester function
+public String[] printArray() {
+	return data;
+}
+}
